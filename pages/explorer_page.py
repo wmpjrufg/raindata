5c5677b03f227fc2951861de29b0febbbcec47f4
Pedro Fernandes <pedrofernandes.olv@gmail.com>
2026-02-07 01:09:02 -0300

    🚚 move dataset functions to specific file

--- pages/explorer_page.py
+++ pages/explorer_page.py
@@ -1,26 +1,15 @@
-import streamlit as st
-import pandas as pd
 import os
 import glob
+
+import pandas as pd
+import streamlit as st
 import plotly.express as px
+
 from utils.i18n import get_text
+from utils.data import load_metadata, load_station_data
+
 
 lang = st.session_state.get("lang")
-
-
-@st.cache_data
-def load_metadata():
-    if os.path.exists("./data/metadata_estacoes.parquet"):
-        try:
-            return pd.read_parquet("./data/metadata_estacoes.parquet")
-        except Exception:
-            return None
-    return None
-
-
-@st.cache_data
-def load_station_data(file_path):
-    return pd.read_parquet(file_path)
 
 
 st.title(get_text('dataset_explorer', lang))
